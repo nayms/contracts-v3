3ba5f87c210637b9df21e2a11f483a1042a9d275
Ramesh Nair <ram@hiddentao.com>
2022-09-16 07:35:51 +0100

    test: nayms ownership facet

---
+++ test/T03NaymsOwnership.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import { D03ProtocolDefaults, console2, LibConstants, LibHelpers, initEntity } from "./defaults/D03ProtocolDefaults.sol";
+
+import { MockAccounts } from "./utils/users/MockAccounts.sol";
+
+import { INayms } from "src/diamonds/nayms/INayms.sol";
+import { Entity } from "src/diamonds/nayms/AppStorage.sol";
+
+import { ERC20 } from "src/erc20/ERC20.sol";
+
+
+contract T03NaymsOwnershipTest is D03ProtocolDefaults, MockAccounts {
+    function setUp() public virtual override {
+        super.setUp();
+    }
+
+    function testTransferOwernshipFailsIfNotContractOwner () public {
+        vm.prank(signer1);
+        vm.expectRevert("LibDiamond: Must be contract owner");
+        nayms.transferOwnership(signer1);
+    }
+
+    function testTransferOwernship () public {
+        nayms.transferOwnership(signer1);
+        
+        assertTrue(nayms.isInGroup(signer1Id, systemContext, LibConstants.GROUP_SYSTEM_ADMINS));
+        assertFalse(nayms.isInGroup(account0Id, systemContext, LibConstants.GROUP_SYSTEM_ADMINS));
+    }
+}
