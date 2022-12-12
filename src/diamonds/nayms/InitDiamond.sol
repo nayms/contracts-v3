84c260c6c6ef63cd83f2d357fb3c76fc9b9d8e1a
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2022-12-12 12:47:32 +0100

    fix(QSP-21): add EIP-165 support

--- src/diamonds/nayms/InitDiamond.sol
+++ src/diamonds/nayms/InitDiamond.sol
@@ -7,6 +7,22 @@ import { LibHelpers } from "./libs/LibHelpers.sol";
 import { LibConstants } from "./libs/LibConstants.sol";
 import { LibAdmin } from "./libs/LibAdmin.sol";
 import { LibACL } from "./libs/LibACL.sol";
+import { LibDiamond } from "../shared/libs/LibDiamond.sol";
+import { IERC165 } from "../shared/interfaces/IERC165.sol";
+import { IDiamondCut } from "../shared/interfaces/IDiamondCut.sol";
+import { IDiamondLoupe } from "../shared/interfaces/IDiamondLoupe.sol";
+import { IERC173 } from "../shared/interfaces/IERC173.sol";
+import { IERC20 } from "../../erc20/IERC20.sol";
+import { IACLFacet } from "../nayms/interfaces/IACLFacet.sol";
+import { IAdminFacet } from "../nayms/interfaces/IAdminFacet.sol";
+import { IEntityFacet } from "../nayms/interfaces/IEntityFacet.sol";
+import { IMarketFacet } from "../nayms/interfaces/IMarketFacet.sol";
+import { INaymsTokenFacet } from "../nayms/interfaces/INaymsTokenFacet.sol";
+import { ISimplePolicyFacet } from "../nayms/interfaces/ISimplePolicyFacet.sol";
+import { ISystemFacet } from "../nayms/interfaces/ISystemFacet.sol";
+import { ITokenizedVaultFacet } from "../nayms/interfaces/ITokenizedVaultFacet.sol";
+import { ITokenizedVaultIOFacet } from "../nayms/interfaces/ITokenizedVaultIOFacet.sol";
+import { IUserFacet } from "../nayms/interfaces/IUserFacet.sol";
 
 error DiamondAlreadyInitialized();
 
@@ -81,6 +97,25 @@ contract InitDiamond {
         s.naymsTokenId = LibHelpers._getIdForAddress(address(this));
         s.naymsToken = address(this);
         s.maxDividendDenominations = 1;
+
+        // adding ERC165 data
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        ds.supportedInterfaces[type(IERC165).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        ds.supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC173).interfaceId] = true;
+        ds.supportedInterfaces[type(IERC20).interfaceId] = true;
+
+        ds.supportedInterfaces[type(IACLFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(IAdminFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(IEntityFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(IMarketFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(INaymsTokenFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(ISimplePolicyFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(ISystemFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(ITokenizedVaultFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(ITokenizedVaultIOFacet).interfaceId] = true;
+        ds.supportedInterfaces[type(IUserFacet).interfaceId] = true;
 
         diamondInitialized = true;
         emit InitializeDiamond(msg.sender, userId);
