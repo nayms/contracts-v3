ae217ca704dc87fbc3621c6d4ffae76f4d31543a
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2023-01-19 17:21:04 +0100

    fix: move signing hash method to the top due to web3j issue

--- src/diamonds/nayms/interfaces/ISimplePolicyFacet.sol
+++ src/diamonds/nayms/interfaces/ISimplePolicyFacet.sol
@@ -10,6 +10,23 @@ import { SimplePolicy, SimplePolicyInfo, PolicyCommissionsBasisPoints } from "./
  */
 interface ISimplePolicyFacet {
     /**
+     * @dev Generate a simple policy hash for singing by the stakeholders
+     * @param _startDate Date when policy becomes active
+     * @param _maturationDate Date after which policy becomes matured
+     * @param _asset ID of the underlying asset, used as collateral and to pay out claims
+     * @param _limit Policy coverage limit
+     * @param _offchainDataHash Hash of all the important policy data stored offchain
+     * @return signingHash_ hash for signing
+     */
+    function getSigningHash(
+        uint256 _startDate,
+        uint256 _maturationDate,
+        bytes32 _asset,
+        uint256 _limit,
+        bytes32 _offchainDataHash
+    ) external view returns (bytes32 signingHash_);
+
+    /**
      * @dev Pay a premium of `_amount` on simple policy
      * @param _policyId Id of the simple policy
      * @param _amount Amount of the premium
@@ -54,21 +71,4 @@ interface ISimplePolicyFacet {
      * @param _policyId Id of the simple policy
      */
     function cancelSimplePolicy(bytes32 _policyId) external;
-
-    /**
-     * @dev Generate a simple policy hash for singing by the stakeholders
-     * @param _startDate Date when policy becomes active
-     * @param _maturationDate Date after which policy becomes matured
-     * @param _asset ID of the underlying asset, used as collateral and to pay out claims
-     * @param _limit Policy coverage limit
-     * @param _offchainDataHash Hash of all the important policy data stored offchain
-     * @return hash for signing
-     */
-    function getSigningHash(
-        uint256 _startDate,
-        uint256 _maturationDate,
-        bytes32 _asset,
-        uint256 _limit,
-        bytes32 _offchainDataHash
-    ) external returns (bytes32);
 }
