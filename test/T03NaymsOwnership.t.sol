26a1eddf676721883a58db5891d2f8015096435e
Kevin Park <kevin@fruitful.gg>
2023-02-08 21:09:09 +0700

    test: add fuzz test for transferOwnership()

--- test/T03NaymsOwnership.t.sol
+++ test/T03NaymsOwnership.t.sol
@@ -29,4 +29,35 @@ contract T03NaymsOwnershipTest is D03ProtocolDefaults, MockAccounts {
         assertTrue(nayms.owner() == signer2);
         assertFalse(nayms.isInGroup(signer2Id, systemContext, LibConstants.GROUP_SYSTEM_ADMINS));
     }
+
+    function testFuzz_TransferOwnership(
+        address newOwner,
+        address notSysAdmin,
+        address anotherSysAdmin
+    ) public {
+        bytes32 notSysAdminId = LibHelpers._getIdForAddress(address(notSysAdmin));
+        assertFalse(nayms.isInGroup(notSysAdminId, systemContext, LibConstants.GROUP_SYSTEM_ADMINS));
+        // 1. Diamond is deployed, owner is set to msg.sender
+        // 2. Diamond cuts in facets and initializes state, a sys admin is set to msg.sender who must be the owner since diamondCut() can only be called by the owner
+
+        // Only a system admin can transfer diamond ownership
+        vm.prank(notSysAdmin);
+        vm.expectRevert("not a system admin");
+        nayms.transferOwnership(newOwner);
+
+        // Only a system admin can transfer diamond ownership, the new owner isn't a system admin
+        vm.prank(newOwner);
+        vm.expectRevert("not a system admin");
+        nayms.transferOwnership(newOwner);
+
+        // System admin can transfer diamond ownership
+        nayms.transferOwnership(newOwner);
+        assertTrue(nayms.owner() == newOwner);
+
+        bytes32 anotherSysAdminId = LibHelpers._getIdForAddress(address(anotherSysAdmin));
+        nayms.assignRole(anotherSysAdminId, systemContext, LibConstants.ROLE_SYSTEM_ADMIN);
+
+        vm.prank(anotherSysAdmin);
+        nayms.transferOwnership(nayms.owner());
+    }
 }
