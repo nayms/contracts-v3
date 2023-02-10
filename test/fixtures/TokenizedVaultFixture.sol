375c63b1256dab1907857b908829c0e60ae5ddbb
Aleksandar Marinković <alexandar.marinkovic@gmail.com>
2023-02-10 15:58:07 +0100

    chore: fix pragmas to version 0.8.17 (#54)

--- test/fixtures/TokenizedVaultFixture.sol
+++ test/fixtures/TokenizedVaultFixture.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13;
+pragma solidity 0.8.17;
 
 import { LibTokenizedVaultIO } from "src/diamonds/nayms/libs/LibTokenizedVaultIO.sol";
 import { LibAdmin } from "src/diamonds/nayms/libs/LibAdmin.sol";
