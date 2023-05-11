4f46a792f30db9cebd69295fcff845dce3af4608
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2023-05-11 15:37:36 +0200

    fix: anvil bootstrap

--- cli-tools/anvil_bootstrap.sh
+++ cli-tools/anvil_bootstrap.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 DIAMOND=$(cat deployedAddresses.json | jq -r '."31337"')
-GTOKEN=0xb7F738c0eb4fEe74DBC9D7949EE5497a01F531bd
+GTOKEN=0xfc12A71BF96d541F439C79E3F16654e1c9B97935
 ACC1=0x2dF0a6dB2F0eF1269bE777C856A7665eeC00649f
 ACC2=0x4C9f9947491c72C668efAA09e033ffe03C976456
 ACC3=0x2328d0d782B9939a982997F2C3F35C2E0f069D86
 ACC4=0x9ADCcEb795F3DBffd66B0b2792225269794C2603
 
-UPGRADE_HASH=$(make anvil-deploy | grep 'upgradeHash: bytes32' | awk '{print $3}')
+make anvil-deploy-diamond
+
+UPGRADE_HASH=$(make anvil-upgrade-sim | grep 'upgradeHash: bytes32' | awk '{print $3}')
 
 make anvil-schedule upgradeHash=$UPGRADE_HASH
 
