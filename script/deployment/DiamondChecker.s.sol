f23318b3b7981a8f067dda817b2bd9954b4212c9
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2023-11-13 09:41:21 +0100

    chore: comment out some old solidity scripts

--- script/deployment/DiamondChecker.s.sol
+++ script/deployment/DiamondChecker.s.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
-import { DiamondProxy } from "src/generated/DiamondProxy.sol";
+// import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
+// import { DiamondProxy } from "src/generated/DiamondProxy.sol";
 
-import "script/utils/DeploymentHelpers.sol";
+// import "script/utils/DeploymentHelpers.sol";
 
-contract DiamondChecker is DeploymentHelpers {
-    /// Does this facet address exist in this diamond?
-    function run(address _facetAddressToCheck, bytes4 _selectorChk) external returns (bool) {
-        IDiamondProxy diamond = IDiamondProxy(address(new DiamondProxy(getDiamondAddressFromFile())));
+// contract DiamondChecker is DeploymentHelpers {
+//     /// Does this facet address exist in this diamond?
+//     function run(address _facetAddressToCheck, bytes4 _selectorChk) external returns (bool) {
+//         IDiamondProxy diamond = IDiamondProxy(address(new DiamondProxy(getDiamondAddressFromFile())));
 
-        address[] memory addresses = diamond.facetAddresses();
+//         address[] memory addresses = diamond.facetAddresses();
 
-        address selectorAddressChk = diamond.facetAddress(_selectorChk);
+//         address selectorAddressChk = diamond.facetAddress(_selectorChk);
 
-        console2.log("facet that selector is associated with: ", selectorAddressChk);
+//         console2.log("facet that selector is associated with: ", selectorAddressChk);
 
-        for (uint256 i; i < addresses.length; i++) {
-            if (addresses[i] == _facetAddressToCheck) {
-                return true;
-            }
-        }
-    }
-}
+//         for (uint256 i; i < addresses.length; i++) {
+//             if (addresses[i] == _facetAddressToCheck) {
+//                 return true;
+//             }
+//         }
+//     }
+// }
--- script/deployment/ReplaceOwnershipFacet.s.sol
+++ script/deployment/ReplaceOwnershipFacet.s.sol
@@ -1,35 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "script/utils/DeploymentHelpers.sol";
+// import "script/utils/DeploymentHelpers.sol";
 
-import { IERC173 } from "src/interfaces/IERC173.sol";
-import { NaymsOwnershipFacet } from "src/facets/NaymsOwnershipFacet.sol";
+// import { IERC173 } from "src/interfaces/IERC173.sol";
+// import { NaymsOwnershipFacet } from "src/facets/NaymsOwnershipFacet.sol";
 
-import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
-import { DiamondProxy } from "src/generated/DiamondProxy.sol";
+// import { IDiamondProxy } from "src/generated/IDiamondProxy.sol";
+// import { DiamondProxy } from "src/generated/DiamondProxy.sol";
 
-contract ReplaceOwnershipFacet is DeploymentHelpers {
-    function run() public returns (bytes32 cutHash) {
-        IDiamondProxy nayms = IDiamondProxy(address(new DiamondProxy(getDiamondAddressFromFile())));
+// contract ReplaceOwnershipFacet is DeploymentHelpers {
+//     function run() public returns (bytes32 cutHash) {
+//         IDiamondProxy nayms = IDiamondProxy(address(new DiamondProxy(getDiamondAddressFromFile())));
 
-        vm.startBroadcast(msg.sender);
-        address addressWithUpgrade = address(new NaymsOwnershipFacet());
+//         vm.startBroadcast(msg.sender);
+//         address addressWithUpgrade = address(new NaymsOwnershipFacet());
 
-        IDiamondCut.FacetCut[] memory cut;
-        cut = new IDiamondCut.FacetCut[](1);
+//         IDiamondCut.FacetCut[] memory cut;
+//         cut = new IDiamondCut.FacetCut[](1);
 
-        bytes4[] memory f0 = new bytes4[](1);
-        f0[0] = IERC173.transferOwnership.selector;
-        cut[0] = IDiamondCut.FacetCut({ facetAddress: address(addressWithUpgrade), action: IDiamondCut.FacetCutAction.Replace, functionSelectors: f0 });
+//         bytes4[] memory f0 = new bytes4[](1);
+//         f0[0] = IERC173.transferOwnership.selector;
+//         cut[0] = IDiamondCut.FacetCut({ facetAddress: address(addressWithUpgrade), action: IDiamondCut.FacetCutAction.Replace, functionSelectors: f0 });
 
-        cutHash = keccak256(abi.encode(cut));
-        if (nayms.getUpgrade(cutHash) > block.timestamp) {
-            nayms.diamondCut(cut, address(0), "");
-        } else {
-            console2.log("Upgrade not allowed - upgrade not registered or upgrade time has passed");
-        }
+//         cutHash = keccak256(abi.encode(cut));
+//         if (nayms.getUpgrade(cutHash) > block.timestamp) {
+//             nayms.diamondCut(cut, address(0), "");
+//         } else {
+//             console2.log("Upgrade not allowed - upgrade not registered or upgrade time has passed");
+//         }
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
