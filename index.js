fbd0ad702ee87b1a3c22a2273ebb807008147a9b
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2023-11-17 11:29:15 +0100

    chore: fix abi path in npm package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  Nayms: require("src/generated/abi.json"),
+  Nayms: require("./src/generated/abi.json"),
   ERC20: require("./forge-artifacts/IERC20.sol/IERC20.json"),
   Constants: require("./forge-artifacts/LibConstants.sol/LibConstants.json"),
   targets: require("./gemforge.deployments.json"),
