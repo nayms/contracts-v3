a1e53739dde82dfb5b812b07cab7788a90b06f3a
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-01-10 13:34:40 +0100

    test: add uniswap fixture

--- src/libs/LibUniswap.sol
+++ src/libs/LibUniswap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import { IERC20 } from "../interfaces/IERC20.sol";
 
@@ -11,6 +11,41 @@ library LibUniswap {
 
     uint160 internal constant MIN_SQRT_RATIO = 4295128739;
     uint160 internal constant MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342;
+
+    function swapExactInputSingleHop(address tokenIn, address tokenOut, uint24 poolFee, uint amountIn) external returns (uint amountOut) {
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(address(router), amountIn);
+
+        bool zeroForOne = tokenIn < tokenOut;
+        uint160 sqrtPriceLimitX96 = zeroForOne ? MIN_SQRT_RATIO + 1 : MAX_SQRT_RATIO - 1;
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: tokenIn,
+            tokenOut: tokenOut,
+            fee: poolFee,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: sqrtPriceLimitX96
+        });
+
+        amountOut = router.exactInputSingle(params);
+    }
+
+    function swapExactInputMultiHop(bytes calldata path, address tokenIn, uint amountIn) external returns (uint amountOut) {
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(address(router), amountIn);
+
+        ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+            path: path,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: 0
+        });
+        amountOut = router.exactInput(params);
+    }
 
     // Example WETH/USDC
     // Sell WETH high      -> Buy WETH low        -> WETH profit
--- test/T06Uniswap.t.sol
+++ test/T06Uniswap.t.sol
@@ -1,22 +1,106 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import { D03ProtocolDefaults, LibHelpers, LibObject, LC, c } from "./defaults/D03ProtocolDefaults.sol";
 import { Vm } from "forge-std/Vm.sol";
 import { StdStyle } from "forge-std/Test.sol";
 import { MockAccounts } from "./utils/users/MockAccounts.sol";
+import { LibUniswap } from "src/libs/LibUniswap.sol";
 
-import { IERC20 } from "src/interfaces/IERC20.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-core/contracts/interfaces/IERC20Minimal.sol";
+import "@uniswap/v3-periphery/contracts/SwapRouter.sol";
 
-contract UniswapV3FlashSwapTest is D03ProtocolDefaults, MockAccounts {
+string constant v3FactoryArtifact = "node_modules/@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
+string constant weth9Artifact = "test/contracts/WETH9.json";
+
+interface WETH9 is IERC20Minimal {
+    function deposit() external payable;
+}
+
+contract UniswapFixture {
+    IUniswapV3Factory public factory;
+    WETH9 public weth9;
+    SwapRouter public router;
+
+    TestERC20[] tokens;
+    NonfungibleTokenPositionDescriptor nftDescriptor;
+    NonfungiblePositionManager nft;
+
+    address wallet = vm.addr(1);
+    address trader = vm.addr(2);
+
+    struct Balances {
+        uint256 weth9;
+        uint256 token0;
+        uint256 token1;
+        uint256 token2;
+    }
+
+    function setUp() public virtual {
+        // Deploys WETH9
+        address _weth9 = deployCode(weth9Artifact);
+        weth9 = WETH9(_weth9);
+
+        // deploy V3 Core's Factory contract
+        address _factory = deployCode(v3FactoryArtifact);
+        factory = IUniswapV3Factory(_factory);
+
+        // Hook them on the router
+        router = new SwapRouter(_factory, _weth9);
+
+        // ---
+        // deploy the 3 tokens
+        address token0 = address(new TestERC20(type(uint256).max / 2));
+        address token1 = address(new TestERC20(type(uint256).max / 2));
+        address token2 = address(new TestERC20(type(uint256).max / 2));
+        require(token0 < token1, "unexpected token ordering 1");
+        require(token2 < token1, "unexpected token ordering 2");
+        // pre-sorted manually, TODO do this properly
+        tokens.push(TestERC20(token1));
+        tokens.push(TestERC20(token2));
+        tokens.push(TestERC20(token0));
+
+        // we don't need to do the lib linking, forge deploys
+        // all libraries and does it for us
+        nftDescriptor = new NonfungibleTokenPositionDescriptor(address(tokens[0]), bytes32("ETH"));
+
+        nft = new NonfungiblePositionManager(address(factory), address(weth9), address(nftDescriptor));
+
+        vm.deal(trader, 100 ether);
+        vm.deal(wallet, 100 ether);
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            tokens[i].approve(address(router), type(uint256).max);
+            tokens[i].approve(address(nft), type(uint256).max);
+            vm.prank(trader);
+            tokens[i].approve(address(router), type(uint256).max);
+            tokens[i].transfer(trader, 1_000_000 * 1 ether);
+        }
+    }
+}
+
+contract UniswapV3FlashSwapTest is D03ProtocolDefaults, MockAccounts, SwapRouterFixture {
     using StdStyle for *;
 
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address private constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address private constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
 
     IERC20 private realWeth = IERC20(WETH);
+    IERC20 private realDai = IERC20(DAI);
+    IERC20 private realUsdc = IERC20(USDC);
 
     function setUp() public {}
+
+    function testSingleHop() public {
+        weth.deposit{ value: 1e18 }();
+        weth.approve(address(this), 1e18);
+
+        uint amountOut = LibUniswap.swapExactInputSingleHop(WETH, DAI, 3000, 1e18);
+
+        c.log("DAI", amountOut);
+    }
 
     function testFlashSwap() public {
         c.log("FLASH".green());
@@ -25,34 +109,5 @@ contract UniswapV3FlashSwapTest is D03ProtocolDefaults, MockAccounts {
         uint24 fee0 = 3000;
         address pool1 = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
         uint24 fee1 = 500;
-
-        // Approve WETH fee
-        uint wethMaxFee = 1e18;
-        c.log("writing balance to: account0".cyan());
-        deal(WETH, account0, wethMaxFee);
-        // vm.startPrank(account0);
-        // writeTokenBalance(account0, naymsAddress, WETH, wethMaxFee);
-        // vm.stopPrank();
-
-        c.log("doing external deposit".cyan());
-        vm.startPrank(account0);
-        nayms.externalDeposit(wethAddress, wethMaxFee);
-        vm.stopPrank();
-
-        // realWeth.deposit{ value: wethMaxFee }();
-        realWeth.approve(address(nayms), wethMaxFee);
-        c.log("WETH approved");
-
-        uint balBefore = realWeth.balanceOf(address(this));
-        c.log("BEFORE flash swap", balBefore);
-        // nayms.flashSwap(pool0, fee1, WETH, USDC, 10 ether);
-        // c.log("AFTER flash swap");
-        // uint balAfter = realWeth.balanceOf(address(this));
-
-        // if (balAfter >= balBefore) {
-        //     c.log("WETH profit", balAfter - balBefore);
-        // } else {
-        //     c.log("WETH loss", balBefore - balAfter);
-        // }
     }
 }
