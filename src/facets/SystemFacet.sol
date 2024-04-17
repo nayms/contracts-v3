9c8bd92e5ccf12032382ddc9e79ebad7deabf34e
Kevin Park <kevin@fruitful.gg>
2024-04-17 14:12:49 +0800

    feat: add emergency locking on {cancel, create} simplePolicy, createEntity

--- src/facets/SystemFacet.sol
+++ src/facets/SystemFacet.sol
@@ -29,7 +29,7 @@ contract SystemFacet is Modifiers, ReentrancyGuard {
         bytes32 _entityAdmin,
         Entity calldata _entityData,
         bytes32 _dataHash
-    ) external assertPrivilege(LibAdmin._getSystemId(), LC.GROUP_SYSTEM_MANAGERS) {
+    ) external notLocked(msg.sig) assertPrivilege(LibAdmin._getSystemId(), LC.GROUP_SYSTEM_MANAGERS) {
         LibEntity._createEntity(_entityId, _entityAdmin, _entityData, _dataHash);
     }
 
