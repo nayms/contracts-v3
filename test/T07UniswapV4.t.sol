b077c95fa1ceeaac02be153c4a1ecbc791a0ab20
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-07-25 13:49:06 +0200

    fix: uniswap v4 project import

--- test/T07UniswapV4.t.sol
+++ test/T07UniswapV4.t.sol
@@ -11,23 +11,24 @@ import { DummyToken } from "./utils/DummyToken.sol";
 import { LibTokenizedVaultStaking } from "src/libs/LibTokenizedVaultStaking.sol";
 import { IERC20 } from "src/interfaces/IERC20.sol";
 
-import { Deployers } from "v4-core/../test/utils/Deployers.sol";
 // import { PoolManager } from "v4-core/PoolManager.sol";
-import { PoolKey } from "v4-core/types/PoolKey.sol";
-import { IHooks } from "v4-core/interfaces/IHooks.sol";
-import { Currency, CurrencyLibrary } from "v4-core/types/Currency.sol";
+import { Deployers } from "@v4-core/test/utils/Deployers.sol";
+import { PoolId, PoolIdLibrary } from "@v4-core/src/types/PoolId.sol";
+import { PoolKey } from "@v4-core/src/types/PoolKey.sol";
+import { IHooks } from "@v4-core/src/interfaces/IHooks.sol";
+import { Currency, CurrencyLibrary } from "@v4-core/src/types/Currency.sol";
 
 contract T07UniswapV4 is D03ProtocolDefaults, Deployers {
     using LibHelpers for address;
     using stdStorage for StdStorage;
     using StdStyle for *;
+    using PoolIdLibrary for PoolKey;
 
     function setUp() public {
         deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
 
-        (currency0, currency1) = deployMintAndApprove2Currencies();
-
-        // uninitializedKey = PoolKey({ currency0: currency0, currency1: currency1, fee: 3000, hooks: IHooks(address(0)), tickSpacing: 60 });
+        PoolKey memory pk = PoolKey({ currency0: currency0, currency1: currency1, fee: 3000, hooks: IHooks(address(0)), tickSpacing: 60 });
     }
 
     function testUniswapV4() public {}
