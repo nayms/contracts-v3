7a120571ec0debfe65ed1e3ff65ff8a5d1f5f6b4
Huan Zhang <huanzhang30@gmail.com>
2024-09-26 16:25:00 +0100

    NAY4-3 Update _isSelfOnboardingApproved to take role as a bytes32 instead of string

--- src/libs/LibAdmin.sol
+++ src/libs/LibAdmin.sol
@@ -226,23 +226,22 @@ library LibAdmin {
         emit FunctionsUnlocked(lockedFunctions);
     }
 
-    function _approveSelfOnboarding(address _userAddress, bytes32 _entityId, string calldata _role) internal {
+    function _approveSelfOnboarding(address _userAddress, bytes32 _entityId, bytes32 _roleId) internal {
         AppStorage storage s = LibAppStorage.diamondStorage();
 
         // The entityId must be the valid type (entity).
         if (!LibObject._isObjectType(_entityId, LC.OBJECT_TYPE_ENTITY)) revert InvalidEntityId(_entityId);
 
         // Require that the user is not approved for the role already
-        bytes32 roleId = LibHelpers._stringToBytes32(_role);
-        if (_isSelfOnboardingApproved(_userAddress, _entityId, _role)) revert EntityOnboardingAlreadyApproved(_userAddress);
+        if (_isSelfOnboardingApproved(_userAddress, _entityId, _roleId)) revert EntityOnboardingAlreadyApproved(_userAddress);
 
-        bool isTokenHolder = roleId == LibHelpers._stringToBytes32(LC.ROLE_ENTITY_TOKEN_HOLDER);
-        bool isCapitalProvider = roleId == LibHelpers._stringToBytes32(LC.ROLE_ENTITY_CP);
+        bool isTokenHolder = _roleId == LibHelpers._stringToBytes32(LC.ROLE_ENTITY_TOKEN_HOLDER);
+        bool isCapitalProvider = _roleId == LibHelpers._stringToBytes32(LC.ROLE_ENTITY_CP);
         if (!isTokenHolder && !isCapitalProvider) {
-            revert InvalidSelfOnboardRoleApproval(_role);
+            revert InvalidSelfOnboardRoleApproval(_roleId);
         }
 
-        s.selfOnboarding[_userAddress] = EntityApproval({ entityId: _entityId, roleId: roleId });
+        s.selfOnboarding[_userAddress] = EntityApproval({ entityId: _entityId, roleId: _roleId });
 
         emit SelfOnboardingApproved(_userAddress);
     }
@@ -278,12 +277,12 @@ library LibAdmin {
         emit SelfOnboardingCompleted(_userAddress);
     }
 
-    function _isSelfOnboardingApproved(address _userAddress, bytes32 _entityId, string calldata _role) internal view returns (bool) {
+    function _isSelfOnboardingApproved(address _userAddress, bytes32 _entityId, bytes32 _roleId) internal view returns (bool) {
         AppStorage storage s = LibAppStorage.diamondStorage();
 
         EntityApproval memory approval = s.selfOnboarding[_userAddress];
 
-        return approval.entityId == _entityId && approval.roleId == LibHelpers._stringToBytes32(_role);
+        return approval.entityId == _entityId && approval.roleId == _roleId;
     }
 
     function _cancelSelfOnboarding(address _userAddress) internal {
